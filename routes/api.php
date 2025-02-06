9634738d6771c6a3991223441f6baebf0bb4ea3b
GiriAdam78 <giridiwa6@gmail.com>
2025-02-06 22:57:40 +0700

    Versi Pertama Dari Api

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+/** 
+ * route "/register"
+ * @method "POST
+*/
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+/**
+ * router "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
